7e172ad976456c2ac698db80e3e7c2f2dbfd2da2
Austin Longo <austinlongocontracting@gmail.com>
2021-11-21 19:10:24 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(guestList) {
+//1) returns an array of thank you messages for each name provided to the function
+    const temp = [];
+    for (let guests = 0; guests < guestList.length; guests ++){
+       temp.push(`Thank you, ${guestList[guests]}, for the wonderful surprise gift!`)
+    }
+    
+    return temp;
+}
+
+function countDown(num){
+//2) invokes console.log once for each number, counting down from the number provided to zero
+//3) logs each number when counting down, starting from the number provided
+   if (num >= 0) {
+    while (num >= 0) {
+        console.log(num);
+        num --;
+        }
+    }
+}
